5caf5290c2cee575b5c468921a64060d2c66134b
karunya1996 <karunya.venkataraman@perficient.com>
2025-11-07 23:35:17 +0530

    Committed repo changes

--- src/main/java/com/example/demo/repo/UserDetailsRepository.java
+++ src/main/java/com/example/demo/repo/UserDetailsRepository.java
@@ -4,9 +4,16 @@ import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.UserDetails;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface UserDetailsRepository extends JpaRepository<UserDetails, Long> {
+	
+	@Query(value="SELECT * FROM user_details WHERE user_name = :userFirstName",nativeQuery=true)
+	List<UserDetails> getUserDetailsByNameNativeQuery(@Param("userFirstName")String userName);
 				
 }
